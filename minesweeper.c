7b5c5b683be8f8c10aa88270390361f64476cfa6
Amitai <git@amitai.ga>
2019-02-11 17:52:31 +0000

    added  cursor

--- minesweeper.c
+++ minesweeper.c
@@ -5,9 +5,8 @@
 
 #include "C:\gbdk\libc\malloc.c" // gbdk is very broken
 
-#define WIDTH 20
-#define HEIGHT 18
-#define MINES 80
+#define BLINK_INTERVAL 32
+#define INPUT_DELAY 150
 
 #define HIDDEN 0
 #define HIDDEN_MINE 1
@@ -18,13 +17,16 @@
 UBYTE width;
 UBYTE height;
 UBYTE mines;
-
 UWORD widthheight;
+UBYTE playerX = 0;
+UBYTE playerY = 0;
 
 UBYTE *board;
 #define BOARD(i, j) (board[i + (width * j)])
 
 #define P(button) (joypad() & button)
+
+#define MOVE_SPRITE(sprite, x, y) move_sprite(sprite, (x + 1) * 8, (y + 2) * 8);
 
 #define DISPLAY(source) set_bkg_tiles(0, 0, width, height, source);
 
@@ -43,6 +45,40 @@ void plantMines(UBYTE *board, UBYTE number) {
 
 }
 
+void waitAndBlink(UBYTE sprite) {
+	
+	UBYTE blink = 1;
+
+	while (!joypad()) {
+		if (clock() % BLINK_INTERVAL == 0) {
+			set_sprite_tile(sprite, (blink = !blink));
+			delay(1);
+		}
+	}
+	set_sprite_tile(sprite, 1);
+
+}
+
+void handleInput() {
+
+	if (P(J_UP)) {
+		playerY--;
+	}
+	if (P(J_DOWN)) {
+		playerY++;
+	}
+	if (P(J_LEFT)) {
+		playerX--;
+	}
+	if (P(J_RIGHT)) {
+		playerX++;
+	}
+
+	MOVE_SPRITE(0, playerX, playerY);
+	delay(INPUT_DELAY);
+
+}
+
 void main() {
 
 	//UBYTE i, j;
@@ -52,7 +88,7 @@ void main() {
 	mines = 80;
 
 	widthheight = width * height;
-	
+
 	board = malloc(widthheight * sizeof(UBYTE));
 
 	waitpad(0xFF);
@@ -64,12 +100,16 @@ void main() {
 
 	set_bkg_data(0, 63, alpha);
 
+	set_sprite_tile(0, 1);
+	MOVE_SPRITE(0, playerX, playerY);
+
 	plantMines(board, mines);
 
 	DISPLAY(board);
 
 	while (1) {
-
+		waitAndBlink(0);
+		handleInput();
 	}
 
 }
